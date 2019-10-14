d433a8463ca263b2f35bdae2a88b39e543860e2c
JCAH <jcah549@gmail.com>
2019-10-13 22:13:57 -0500

    primer commit

---
+++ villalpando/src/main/java/com/jcah/villalpando/VillalpandoApplication.java
@@ -0,0 +1,13 @@
+package com.jcah.villalpando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillalpandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillalpandoApplication.class, args);
+	}
+
+}
---
+++ villalpando/src/test/java/com/jcah/villalpando/VillalpandoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jcah.villalpando;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VillalpandoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
